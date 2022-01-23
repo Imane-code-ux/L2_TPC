5eb2f5d94e52f88295a1167a3771be789b77f6df
imane-code-ux <imane.baladi@um6p.ma>
2022-01-23 02:39:59 +0100

    code

--- Tp_Optionnel/solution.c
+++ Tp_Optionnel/solution.c
@@ -1,1 +1,25 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>  
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
 
+char* get_next_line(int fd){
+
+    int maxLine = 120;
+    char* buf = (char*)malloc(sizeof(char)*maxLine);
+    int x = read(fd,buf,maxLine);
+    return buf;
+}
+
+int main(){
+    int fd = open("text", O_RDONLY);
+    char* line;
+    //for(int i = 0 ; i < )
+    line = get_next_line(fd);
+    printf("%s",line);
+    line = get_next_line(fd);
+    printf("%s",line);
+    return 0;
+}
